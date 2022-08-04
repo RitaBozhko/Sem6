5bda1ee6da1e6d4de6e0850c3310e466af6a2354
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-08-04 18:14:00 +0300

    Homework Seminar6

---
+++ Homework_task_41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+int m = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+for (int i=0; i<m; i++)
+{
+    Console.WriteLine("Введите число: ");
+    bool userEnter = int.TryParse(Console.ReadLine(), out int userNum);
+    if (userNum > 0) count ++;
+    if(!userEnter) break;
+}
+
+Console.WriteLine($"Вы ввели {count} чисел больше нуля.");
---
+++ Homework_task_43_WhereLinesCross/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+Console.WriteLine("Введите 4 числа, они станут коэффициентами двух линейных уравнений.");
+Console.WriteLine("Введите число: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число: ");
+double k2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число: ");
+double b2 = double.Parse(Console.ReadLine()!);
+
+
+void FindWhereLinesCross(double k1, double b1, double k2, double b2)
+{
+    if (k1==k2) Console.WriteLine("Прямые параллельны, точек пересечения нет.");
+    else
+    {
+        double x = (b2 - b1)/(k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Прямые пересекутся в точке с координатами ({x}, {y}).");
+    }
+}
+
+FindWhereLinesCross(k1,b1,k2,b2);
